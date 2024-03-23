44e4d02776b797c5c6d18e6ab3a647638e2eabee
shorv <kontakt.shorv@gmail.com>
2024-03-23 21:09:11 +0100

    initial commit

---
+++ src/main/java/io/github/butexbackend/ButexBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.butexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButexBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ButexBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/butexbackend/ButexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.butexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
